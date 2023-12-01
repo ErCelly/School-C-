cc0eff36670520629ee9a0e7f4180e1ea54a8b79
ErCelly <152600787+ErCelly@users.noreply.github.com>
2023-12-01 10:35:41 +0100

    upload "Hello world"

---
+++ Programma For e Array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	
+	const int [DIM]=5;
+	int v[DIM];
+	for (int x=0; z<DIM;x++) {
+		v[x]=x;
+	    
+	}
+	
+	return 0 //non funziona bro, sei scarso
+}
